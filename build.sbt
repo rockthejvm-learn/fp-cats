d174595452f2e0ccc455ef06278863865a660bf2
Vladimir Bystrov <vlad.bystrov@gmail.com>
2021-03-20 14:02:09 +0300

    initial setup

---
+++ build.sbt
@@ -0,0 +1,16 @@
+ThisBuild / organization := "com.udavpit.fp.cats"
+ThisBuild / version      := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.4"
+ThisBuild / scalacOptions ++= Seq(
+  "-deprecation",
+  "-language:postfixOps"
+)
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "fp-cats",
+    libraryDependencies ++= Seq(
+      "org.typelevel" %% "cats-core" % "2.3.0"
+    )
+  )
---
+++ src/main/scala/com/udavpit/fp/cats/intro/TypeClasses.scala
@@ -0,0 +1,47 @@
+package com.udavpit.fp.cats.intro
+
+object TypeClasses {
+
+  case class Person(name: String, age: Int)
+
+  // part 1 - type class definition
+  trait JSONSerializer[T] {
+    def toJson(value: T): String
+  }
+
+  // part 2 - create implicit type class INSTANCES
+  implicit object StringSerializer extends JSONSerializer[String] {
+    override def toJson(value: String): String = "\"" + value + "\""
+  }
+
+  implicit object IntSerializer extends JSONSerializer[Int] {
+    override def toJson(value: Int): String = value.toString
+  }
+
+  implicit object PersonSerializer extends JSONSerializer[Person] {
+    override def toJson(value: Person): String =
+      s"""
+         |{ "name" : ${value.name}, "age" : ${value.age} }
+         |""".stripMargin.trim
+  }
+
+  // part 3 - offer some API
+  def convertListToJSON[T](list: List[T])(implicit serializer: JSONSerializer[T]): String =
+    list.map(v => serializer.toJson(v)).mkString("[", ",", "]")
+
+  // part 4 - extending the existing types via extension methods
+  object JSONSyntax {
+    implicit class JSONSerializable[T](value: T)(implicit serializer: JSONSerializer[T]) {
+      def toJson: String = serializer.toJson(value)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(convertListToJSON(List(Person("Alice", 23), Person("Xavier", 45))))
+
+    import JSONSyntax._
+
+    val bob = Person("Bob", 35)
+    println(bob.toJson)
+  }
+}
